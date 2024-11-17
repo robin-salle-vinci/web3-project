02ff19a5b63b58190c5b01db5a6bf1d8a174f563
Sallé Robin <robin.salle@student.vinci.be>
2024-11-17 15:02:28 +0100

    start README

---
+++ README.md
@@ -0,0 +1,4 @@
+BUT FONCTIONNEL DE L'APPLICATION
+
+L'objectif de notre application Discord bot est d'offrir diverses commandes utilitaires 
+
--- index.js
+++ index.js
@@ -11,11 +11,14 @@ client.once('ready', () => {
 
 client.on('messageCreate', async message => {
     if(message.content === '!ping') message.reply('!pong')
+        
     if(message.content.startsWith('!chat')){
         const request = message.content.slice(6)
         const response = await fetchAI(request)
         message.reply(response)
     }
+
+
 });
 
 
